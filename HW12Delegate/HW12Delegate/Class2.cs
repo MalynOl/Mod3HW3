c61b647b1d3260c582a4ddb73506c4a2dca276d6
Aleksandra <oleksandramalyn@gmail.com>
2022-02-21 18:08:09 +0200

    add program

---
+++ HW12Delegate/HW12Delegate/Class2.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW12Delegate
+{
+    internal class Class2
+    {
+        public int ResultPow { get; set; }
+
+        public Predicate<int> Calc(Func<int, int, int> func, int number1, int number2)
+        {
+            Class1 class1 = new Class1();
+            func = class1.Pow;
+            ResultPow = func(number1, number2);
+            Predicate<int> handlerResult = Result;
+
+            return handlerResult;
+        }
+
+        public bool Result(int number)
+        {
+            int ostdelen = ResultPow % number;
+
+            if (ostdelen == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ HW12Delegate/HW12Delegate/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace HW12Delegate
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Class1 class1 = new Class1();
+            Class2 class2 = new Class2();
+            var handlerResult = class2.Calc(class1.Pow, 12, 4);
+            var b = handlerResult(6);
+            class1.HandlerShow(b);
+        }
+
+        public void Show(bool b)
+        {
+            Console.WriteLine(b.ToString());
+        }
+    }
+}
